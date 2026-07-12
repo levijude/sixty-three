1454255894ce5d14407c2b544cdd6113759b9d53
levijude <168497701+levijude@users.noreply.github.com>
2026-07-11 22:32:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let stuff = 0
+const stuffthing = document.getElementById("stuff")
+function updatestuff() {
+		stuffthing.textContent = `stuff: ${stuff}`;
+}
+clickButton.addEventListener("click", (event) => {
+	score += clickValue;
+	updateScore();
