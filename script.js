d5aeb6de1a75c24aeb8269691be0825ecf7e76d8
levijude <168497701+levijude@users.noreply.github.com>
2026-07-11 23:12:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let stuff = 0
 let clickValue = 1;
+let stuffbetterers = 0;
 const stuffthing = document.getElementById("stuffamt")
+const sbthing = document.getElementById("
 const stuffbutton = document.getElementById("stuff");
 function updatestuff() {
 		stuffthing.textContent = `stuff: ${stuff}`;
