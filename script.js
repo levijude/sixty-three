4bc0e6befadbaca0b26d892435cd594fa027b8c4
levijude <168497701+levijude@users.noreply.github.com>
2026-07-11 23:26:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let stuff = 0
-let clickValue = 1;
 let sbvalue = 1;
 let stuffbetterers = 0;
 let sbcost = 50;
+let sbmult = 1;
+let sbmultinc = 0.25;
+let clickValue = 1 * sbmult;
 const stuffthing = document.getElementById("stuffamt");
 const sbthing = document.getElementById("sb");
 const stuffbutton = document.getElementById("stuff");
@@ -11,17 +13,19 @@ function updatestuff() {
 		stuffthing.textContent = `stuff: ${stuff}`;
 }
 function updatesb() {
-		sbthing.textContent = `Stuff Betterers: ${stuffbetterers} multiplying stuff gain by x${stuffbetterers/4}`;
+		sbthing.textContent = `Stuff Betterers: ${stuffbetterers} multiplying stuff gain by x${sbmult}`;
 }
 stuffbutton.addEventListener("click", (event) => {
 	stuff += clickValue;
 	updatestuff();
 }) 
 sbbutton.addEventListener("click", (event) => {
-	if stuff >= sbcost {
+	if (stuff >= sbcost) {
 		stuff -= sbcost;
 		stuffbetterers += sbvalue;
+		sbcost *= 1.333;
+		sbmult += sbmultinc;
 		sbbutton.textContent = `purchase a Stuff Betterer for ${sbcost} stuff`;
+		updatesb();
 	}
-	updatesb();
 }) 
