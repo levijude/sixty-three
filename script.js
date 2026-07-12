921cb956d5138ae914c8637809c8ffcc1fce1917
levijude <168497701+levijude@users.noreply.github.com>
2026-07-11 23:48:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let sbvalue = 1;
 let stuffbetterers = 0;
 let sbcost = 50;
 let sbmult = 1;
-let sbmultinc = 0.25;
+let sbmultinc = 0.75;
 let clickValue = 1;
 const stuffthing = document.getElementById("stuffamt");
 const sbthing = document.getElementById("sb");
 const stuffbutton = document.getElementById("stuff");
 const sbbutton = document.getElementById("stuffbetterer");
 function updatestuff() {
-		stuffthing.textContent = `stuff: ${stuff}`;
+		stuffthing.textContent = `stuff: ${Math.round(stuff)}`;
 }
 function updatesb() {
 		sbthing.textContent = `Stuff Betterers: ${stuffbetterers} multiplying stuff gain by x${sbmult}`;
@@ -23,9 +23,9 @@ sbbutton.addEventListener("click", (event) => {
 	if (stuff >= sbcost) {
 		stuff -= sbcost;
 		stuffbetterers += sbvalue;
-		sbcost *= 1.333;
+		sbcost *= 1.64;
 		sbmult += sbmultinc;
-		sbbutton.textContent = `purchase a Stuff Betterer for ${sbcost} stuff`;
+		sbbutton.textContent = `purchase a Stuff Betterer for ${Math.round(sbcost)} stuff`;
 		updatestuff();
 		updatesb();
 	}
